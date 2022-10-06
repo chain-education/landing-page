e42775cd334ed7e43bd91ede5357072e5c60bbac
Muskan Marwah <marwahmuskan330@gmail.com>
2022-10-06 07:10:35 +0530

    [Muskan] Updated Hero component

--- landing-page/components/Hero.js
+++ landing-page/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
           <Col lg={6}>
             <div className="pr-lg-5">
               <p className="text-uppercase text-primary font-weight-medium f-14 mb-4">Lorem Ipsum</p>
-              <h1 className="mb-4 font-weight-normal line-height-1_4">Simply dummy text of the printing <span className="text-primary font-weight-medium">Name</span></h1>
+              <h1 className="mb-4 font-weight-normal line-height-1_4">Welcome to ChainEducation <span className="text-primary font-weight-medium">Name</span></h1>
               <p className="text-muted mb-4 pb-2">I dont know what to write here. Muskan kindly do something.</p>
               <a href="#" className="btn btn-warning">
                 Find Out How <span className="ml-2 right-icon">&#8594;</span>
