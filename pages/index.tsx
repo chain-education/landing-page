135b3e9eefc80132055dd850e20f79782d93f932
Muskan Marwah <marwahmuskan330@gmail.com>
2022-10-21 03:23:58 +0530

    [Aviraj|Muskan] Added few pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
 import Layout from "../components/Layout";
 import Header from "../components/Header";
-import Classrooms from "../components/Classrooms";
+import Info from "../components/Info";
 import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <Layout pageTitle="ChainEducation | Home">
       <Header />
-      <Classrooms />
+      <Info />
       <Footer />
     </Layout>
   )
